c3795a5e614af5ce01e90328dc381ba66b6a7adf
ania0716 <andrii.iashchuk@netcracker.com>
2018-04-11 18:59:56 +0300

    performance was improved

--- src/app/components/table.component.js
+++ src/app/components/table.component.js
@@ -2,41 +2,61 @@ import PropTypes from 'prop-types';
 import * as React from 'react';
 import { getTypeToImageMapper } from '../utils/utils';
 
-function TableComponent(props) {
-    const mapper = getTypeToImageMapper();
-    const { name, warning, type, maxPlayers, id, unsetWarning, players } = props;
-    return (
-        <div className={`table ${warning ? "warning" : ""}`}>
-            <div className="header">
+
+class TableComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if(nextProps){
+            if (this.props.warning !== nextProps.warning) {
+                return true;
+            }
+            if (this.props.players !== nextProps.players) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    render(){
+        const mapper = getTypeToImageMapper();
+        const { name, warning, type, maxPlayers, id, unsetWarning, players } = this.props;
+        return (
+            <div className={`table ${warning ? "warning" : ""}`}>
+                <div className="header">
                 <span className="name-string">
                     Name:
                 </span>
-                <span className="name">
+                    <span className="name">
                     {name || ''}
                 </span>
-            </div>
-            <div className="image">
-                <img alt="table_img" src={mapper[type]} />
-            </div>
-            <div>
-                <div className="info">
-                    <span>Players:</span>
-                    <span className="bold">{players}</span>
                 </div>
-                <div className="info">
-                    <span>MaxPlayers:</span>
-                    <span className="bold">{maxPlayers}</span>
+                <div className="image">
+                    <img alt="table_img" src={mapper[type]} />
                 </div>
-                {warning ?
-                    (<div className="info">
-                        <button onClick={() => unsetWarning(id)}>
-                            Fix warning
-                        </button>
-                    </div>)
-                    : null}
+                <div>
+                    <div className="info">
+                        <span>Players:</span>
+                        <span className="bold">{players}</span>
+                    </div>
+                    <div className="info">
+                        <span>MaxPlayers:</span>
+                        <span className="bold">{maxPlayers}</span>
+                    </div>
+                    {warning ?
+                        (<div className="info">
+                            <button onClick={() => unsetWarning(id)}>
+                                Fix warning
+                            </button>
+                        </div>)
+                        : null}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 TableComponent.propTypes = {
